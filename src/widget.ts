b53b20f7bb4b641cbc83465d1469ac194996994c
Ginzhio <pandemicrestart@gmail.com>
2025-04-23 17:54:12 +0200

    shake to reveal

--- src/widget.ts
+++ src/widget.ts
@@ -20,12 +20,14 @@ const widgetStyles = `
     --neo-captcha-bg: var(--neo-captcha-bg-light);
     --neo-captcha-bg2: var(--neo-captcha-bg2-light);
     --neo-captcha-fg: var(--neo-captcha-fg-light);
+    --neo-captcha-gradient: linear-gradient(color-mix(in srgb, var(--neo-captcha-fg) 2%, transparent), color-mix(in srgb, var(--neo-captcha-fg) 15%, transparent));
 }
 
 .neo-captcha-theme-dark {
     --neo-captcha-bg: var(--neo-captcha-bg-dark);
     --neo-captcha-bg2: var(--neo-captcha-bg2-dark);
     --neo-captcha-fg: var(--neo-captcha-fg-dark);
+    --neo-captcha-gradient: linear-gradient(color-mix(in srgb, var(--neo-captcha-fg) 25%, transparent), color-mix(in srgb, var(--neo-captcha-fg) 5%, transparent));
 }
 
 .neo-captcha-box {
@@ -192,6 +194,7 @@ const widgetStyles = `
 .neo-captcha-signal-text {
     top: 0.5em;
     font-size: 1.5em;
+    color: var(--neo-captcha-light);
     text-shadow: #000 0 0 0.25em;
     position: absolute;
 }
@@ -209,6 +212,19 @@ const widgetStyles = `
         opacity: 20%;
     }
 }
+
+@keyframes shake {
+    0% {
+        transform: rotate(-10deg) translateX(-0.25em);
+    }
+    50% {
+        transform: rotate(10deg) translateX(0.25em);
+    }
+    100% {
+        transform: rotate(-10deg) translateX(-0.25em);
+    }
+}
+
 
 .neo-captcha-fg-icon {
     font-size: 3em;
@@ -261,7 +277,7 @@ const widgetStyles = `
     font-weight: bold;
     cursor: pointer;
     padding: 0.6em 1em 0.4em 1em;
-    background: linear-gradient(color-mix(in srgb, var(--neo-captcha-fg) 25%, transparent), color-mix(in srgb, var(--neo-captcha-fg) 5%, transparent));
+    background: var(--neo-captcha-gradient);
     display: flex;
     flex-direction: row;
     margin: 0;
@@ -504,7 +520,8 @@ export function renderCaptcha(target: HTMLElement) {
         howto: string,
         step_1: string,
         step_2: string,
-        step_2_s: string,
+        step_2_desktop: string,
+        step_2_motion: string,
         step_3: string,
         mode_1: string,
         mode_1_text: string,
@@ -523,7 +540,8 @@ export function renderCaptcha(target: HTMLElement) {
             howto: '?   How-To:',
             step_1: 'Hit ▶ Play',
             step_2: `Tap when <b><span style="color: rgba(0, 160, 0)">GREEN</span>!<b/>`,
-            step_2_s: `Click at the <b>signal tone!</b>`,
+            step_2_desktop: `Click at the <b>signal tone!</b>`,
+            step_2_motion: `<b>Shake</b> your phone!`,
             step_3: '<b>Solve the CAPTCHA</b>',
             mode_1: 'Implied square:',
             mode_1_text: 'Mark the missing corner!',
@@ -542,7 +560,8 @@ export function renderCaptcha(target: HTMLElement) {
             howto: '?   Wie man\'s macht:',
             step_1: 'Drücke ▶ Start',
             step_2: `Tippe bei <b><span style="color: rgba(0, 160, 0)">GRÜN</span>!<b/>`,
-            step_2_s: 'Klicke beim <b>Signalton!</b>',
+            step_2_desktop: 'Klicke beim <b>Signalton!</b>',
+            step_2_motion: '<b>Schüttel</b> dein Handy!',
             step_3: '<b>Löse das CAPTCHA!</b>',
             mode_1: 'Angedeutetes Viereck:',
             mode_1_text: 'Markiere die fehlende Ecke!',
@@ -564,8 +583,8 @@ export function renderCaptcha(target: HTMLElement) {
         document.getElementById("neoCaptcha-step_2")!.innerHTML = (translations[userLang] || translations['en']).step_2;
         document.getElementById("neoCaptcha-signalText")!.innerHTML = (translations[userLang] || translations['en']).step_2;
     } else {
-        document.getElementById("neoCaptcha-step_2")!.innerHTML = (translations[userLang] || translations['en']).step_2_s;
-        document.getElementById("neoCaptcha-signalText")!.innerHTML = (translations[userLang] || translations['en']).step_2_s;
+        document.getElementById("neoCaptcha-step_2")!.innerHTML = (translations[userLang] || translations['en']).step_2_desktop;
+        document.getElementById("neoCaptcha-signalText")!.innerHTML = (translations[userLang] || translations['en']).step_2_desktop;
     }
     document.getElementById("neoCaptcha-step_3")!.innerHTML = (translations[userLang] || translations['en']).step_3;
     if (variantNs) {
@@ -623,6 +642,20 @@ export function renderCaptcha(target: HTMLElement) {
     let thumbSize: number = 0;
     let challenge: string | undefined = undefined;
     let hmac: string | undefined = undefined;
+    let suspense: number = 0;
+
+// stuff for shake
+    const motionThrottle = 50;
+    const minAccs = 500 / motionThrottle; // half a second of data points
+    const alpha = 0.6; // higher for more smoothing
+    let lastMotionTime = 0;
+    let smoothX = 0, smoothY = 0, smoothZ = 0;
+    let lastAcc: {
+        mag: number, move: number, x: number, y: number, z: number,
+        dmag: number, dx: number, dy: number, dz: number
+    } | undefined = undefined;
+    let accs: any[] = [];
+    let motionEnabled = false;
 
     let howToShown = true;
     let howToExpanded = false;
@@ -689,6 +722,7 @@ export function renderCaptcha(target: HTMLElement) {
             challenge = result.challenge;
             hmac = result.hmac;
             totalTime = result.totalTime || totalTime;
+            suspense = result.suspense;
             const container = document.getElementById("neoCaptcha-container") as HTMLDivElement;
             container.style.height = "20em";
             if (result.variant === 'ns') {
@@ -714,7 +748,196 @@ export function renderCaptcha(target: HTMLElement) {
             bar.fillStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]})`;
 
             idleStartTime = Date.now();
-            setTimeout(() => beep(), result.suspense);
+
+            if (isMobile && window.DeviceMotionEvent) {
+                // request permission first (if needed)
+                if ('requestPermission' in DeviceMotionEvent) {
+                    (DeviceMotionEvent as any).requestPermission()
+                        .then((response: any) => {
+                            if (response === 'granted') {
+                                window.addEventListener('devicemotion', handleMotion);
+                                console.log("beepIfNoMotion timeout, permission");
+                                setTimeout(() => beepIfNoMotion(), 1000);
+                            } else {
+                                console.log("beep timeout, no motion permission");
+                                setTimeout(() => beep(), suspense);
+                            }
+                        })
+                        .catch(console.error);
+                } else {
+                    window.addEventListener('devicemotion', handleMotion);
+                    console.log("beepIfNoMotion timeout, regular");
+                    setTimeout(() => beepIfNoMotion(), 1000);
+                }
+            } else {
+                console.log("beep timeout, no motion available");
+                setTimeout(() => beep(), suspense);
+            }
+        }
+    }
+
+    function setShakeEnabled(enabled: boolean) {
+        motionEnabled = enabled;
+        if (isMobile) {
+            if (motionEnabled) {
+                overlayBg.style.background = mobileRed;
+                signalIcon.innerText = "edgesensor_low";
+                signalIcon.style.animation = "shake 0.4s ease-in-out infinite";
+                document.getElementById("neoCaptcha-step_2")!.innerHTML = (translations[userLang] || translations['en']).step_2_motion;
+                document.getElementById("neoCaptcha-signalText")!.innerHTML = (translations[userLang] || translations['en']).step_2_motion;
+                overlay.removeEventListener("pointerdown", react);
+                overlay.removeEventListener("pointermove", consumeMove);
+                overlay.removeEventListener("pointerup", start);
+            } else {
+                overlayBg.style.background = mobileRed;
+                signalIcon.innerText = "do_not_touch";
+                signalIcon.style.animation = "none";
+                document.getElementById("neoCaptcha-step_2")!.innerHTML = (translations[userLang] || translations['en']).step_2;
+                document.getElementById("neoCaptcha-signalText")!.innerHTML = (translations[userLang] || translations['en']).step_2;
+                overlay.addEventListener("pointerdown", react, {passive: false});
+                overlay.addEventListener("pointermove", consumeMove, {passive: false});
+                overlay.addEventListener("pointerup", start);
+            }
+        }
+    }
+
+    async function handleMotion(event: DeviceMotionEvent) {
+        if (!motionEnabled) console.log("handleMotion");
+        if (!(event instanceof DeviceMotionEvent)) return;
+
+        // throttle
+        const now = Date.now();
+        if (lastMotionTime > 0 && now - lastMotionTime < motionThrottle) return;
+        if (!motionEnabled) console.log("there is motion");
+        lastMotionTime = now;
+
+        const acc = event.accelerationIncludingGravity;
+        if (acc && acc.x !== null && acc.y !== null && acc.z !== null) {
+            if (!motionEnabled) {
+                setShakeEnabled(true);
+            }
+            // simple low-pass filter (weighted average)
+            smoothX = alpha * smoothX + (1 - alpha) * acc.x;
+            smoothY = alpha * smoothY + (1 - alpha) * acc.y;
+            smoothZ = alpha * smoothZ + (1 - alpha) * acc.z;
+            let mag = Math.sqrt(smoothX * smoothX + smoothY * smoothY + smoothZ * smoothZ);
+            let move = Math.sqrt(smoothX * smoothX + smoothY * smoothY);
+            if (lastAcc) {
+                let dmag = mag - lastAcc.mag;
+                let dx = smoothX - lastAcc.x;
+                let dy = smoothY - lastAcc.y;
+                let dz = smoothZ - lastAcc.z;
+                lastAcc = {
+                    mag: mag,
+                    move: move,
+                    x: smoothX,
+                    y: smoothY,
+                    z: smoothZ,
+                    dmag: dmag,
+                    dx: dx,
+                    dy: dy,
+                    dz: dz
+                };
+                accs.push(lastAcc);
+                if (accs.length > minAccs) {
+                    if (beepStartTime <= 0) beepStartTime = Date.now();
+                    if (evaluateShake()) {
+                        react();
+                        start();
+                    }
+                }
+            } else {
+                lastAcc = {mag: mag, move: move, x: smoothX, y: smoothY, z: smoothZ, dmag: 0, dx: 0, dy: 0, dz: 0};
+            }
+        }
+    }
+
+    function evaluateShake(logs: boolean = false) {
+        const g = 9;
+        const minMag = 7;
+
+        let dir = 0;
+        let consecutive = 0;
+        let sumMag = 0;
+        let minMove = 99
+        let maxMove = -99;
+
+        function resetShakeVals() {
+            dir = 0;
+            consecutive = 0;
+            sumMag = 0;
+            minMove = 99
+            maxMove = -99;
+        }
+
+        let shakes = 0;
+        let i = 0;
+        let last: any;
+        for (const acc of accs) {
+            i++;
+            if (i < minAccs) {
+                last = acc;
+                continue;
+            }
+
+            let directionChanged = false;
+            if (Math.abs(acc.x) > 2) {
+                if (acc.x < last.x) {
+                    // move left
+                    if (logs) console.log(i - minAccs, "<left", "x:", acc.x, "move:", acc.move, "mag:", acc.mag);
+
+                    if (dir === 1) directionChanged = true
+                    else consecutive++;
+                    dir = -1;
+
+                } else {
+                    // move right
+                    if (logs) console.log(i - minAccs, "right>", "x:", acc.x, "move:", acc.move, "mag:", acc.mag);
+
+                    if (dir === -1) directionChanged = true
+                    else consecutive++;
+                    dir = 1;
+
+                }
+                sumMag += acc.mag;
+                minMove = Math.min(minMove, Math.sign(acc.x) * acc.move);
+                maxMove = Math.max(maxMove, Math.sign(acc.x) * acc.move);
+            } else if (acc.mag > g) {
+                if (logs) console.log(i - minAccs, "idle");
+                resetShakeVals();
+                shakes = 0;
+            }
+            // detect shake
+            if (directionChanged) {
+                let validMoveLength = 2 <= consecutive && consecutive <= 5;
+                let avgMag = sumMag / consecutive;
+                let dMove = Math.abs(maxMove - minMove);
+                if (validMoveLength && avgMag > minMag && dMove > minMag) {
+                    shakes++;
+                }
+                resetShakeVals();
+            }
+
+            last = acc;
+            if (shakes >= 2) break;
+        }
+        if (shakes >= 1) {
+            // shaking
+            overlayBg.style.background = mobileGreen;
+        } else if (dir === 0) {
+            // idle
+            overlayBg.style.background = mobileRed;
+            beepStartTime = Date.now();
+        }
+        return shakes >= 2;
+    }
+
+    function beepIfNoMotion() {
+        if (!motionEnabled || lastMotionTime <= 0) {
+            console.log("beep timeout, regular", "motionEnabled:", motionEnabled, "lastMotionTime:", lastMotionTime);
+            setTimeout(() => beep(), suspense);
+        } else {
+            console.log("no beep", "motionEnabled:", motionEnabled, "lastMotionTime:", lastMotionTime);
         }
     }
 
@@ -734,7 +957,7 @@ export function renderCaptcha(target: HTMLElement) {
 
     const playTone = () => {
         if (audio.state === "suspended") {
-            // important for iOS/Chrome
+            // Wichtig für iOS/Chrome
             audio.resume().then(() => actuallyPlayTone());
         } else {
             actuallyPlayTone();
@@ -779,11 +1002,13 @@ export function renderCaptcha(target: HTMLElement) {
     }
 
     overlay.addEventListener("pointerdown", react, {passive: false});
-    overlay.addEventListener("pointermove", () => {
+    overlay.addEventListener("pointermove", consumeMove, {passive: false});
+
+    function consumeMove() {
         if (beepStartTime <= 0) {
             reaction = undefined;
         }
-    }, {passive: false});
+    }
 
     function start() {
         if (beepStartTime <= 0) {
@@ -793,6 +1018,11 @@ export function renderCaptcha(target: HTMLElement) {
         if (reaction && reaction.time === 0) return;
 
         if (beepStartTime > 0 && startTime == 0 && reaction) {
+            if (lastMotionTime > 0) {
+                window.removeEventListener('devicemotion', handleMotion);
+                evaluateShake(true);
+            }
+
             activity.push(reaction);
             activity.push({action: "start", time: Date.now() - idleStartTime});
 
@@ -1159,10 +1389,20 @@ export function renderCaptcha(target: HTMLElement) {
             bar.clearRect(0, 0, timeCanvas.width, timeCanvas.height);
         }
         if (isMobile) {
-            overlayBg.style.background = mobileRed;
-            signalIcon.innerText = "do_not_touch";
+            if (motionEnabled) {
+                lastMotionTime = 0;
+                smoothX = 0;
+                smoothY = 0;
+                smoothZ = 0;
+                lastAcc = undefined;
+                accs = [];
+                setShakeEnabled(true);
+            } else {
+                overlayBg.style.background = mobileRed;
+                signalIcon.innerText = "do_not_touch";
+                signalIcon.style.animation = "none";
+            }
         }
-        signalIcon.style.animation = "none";
         document.getElementById("neoCaptcha-guess")!.style.display = "grid";
         document.getElementById("neoCaptcha-submit")!.style.display = "block";
         if (variantNs) {
